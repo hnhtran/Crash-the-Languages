c3efd99bfda2eff46f96c8eab9fdbef26f08012f
hnhtran <tran_eetas@yahoo.com>
2022-05-25 00:22:46 +1000

    event when card clicked

--- app.js
+++ app.js
@@ -21,13 +21,28 @@ const shuffle = (arr) => {
 // Generate meaning at the back of the cards
 let cardsBack = [];
 const cardsBackFill = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < cardE.length; j++) {
-      if (arr[i] === cardE[j]) {
-        cardsBack.push(cardV[j]);
+  // bug here, when call this function everytime, its generate a new array to add on the previous one. resolved!
+  if (cardsBack === null) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < cardE.length; j++) {
+        if (arr[i] === cardE[j]) {
+          cardsBack.push(cardV[j]);
+        }
+        if (arr[i] === cardV[j]) {
+          cardsBack.push(cardE[j]);
+        }
       }
-      if (arr[i] === cardV[j]) {
-        cardsBack.push(cardE[j]);
+    }
+  } else {
+    cardsBack = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < cardE.length; j++) {
+        if (arr[i] === cardE[j]) {
+          cardsBack.push(cardV[j]);
+        }
+        if (arr[i] === cardV[j]) {
+          cardsBack.push(cardE[j]);
+        }
       }
     }
   }
@@ -36,35 +51,60 @@ const cardsBackFill = (arr) => {
 // cardsFill will be called when Start button is pressed, and empty table will be filled with words
 const cardsFill = () => {
   shuffledCards = shuffle(cards);
-  console.log(
-    `${shuffledCards} First display cards when Start button pressed`
-  );
+  // console.log(
+  //   `${shuffledCards} First display cards when Start button pressed`
+  // );
   for (let i = 0; i < shuffledCards.length; i++) {
     cardsTable[i].innerText = shuffledCards[i];
   }
   // everytime cardFill was called, cardsBackFill should be called also
   cardsBackFill(shuffledCards);
-  console.log(`${cardsBack} Back of the cards was called `);
+  // console.log(`${cardsBack} Back of the cards was called `);
+};
+
+const cardClicked = (e) => {
+  let cardId = e.target.id;
+  console.log(
+    `${cardId} clicked, with the word ${cardsTable[cardId].innerText}`
+  ); // cannot do cardId.innerText, maybe not exist.. shoudl be cardsTable[id]?
+
+  // console.log(`${cardsBack} Back of the cards was called `);
+  // console.log(`${shuffledCards} First display cards when Start button pressed`);
+
+  // logic here.
+  // also, bug here. When 1 click, all of the card will be changed backwards, except that card
+  // that text will be stored in temp
+  // reset that text to cardsBack value
+  // temp will be used for if statement (or isMatch())
+  // what is isMatch?
+  let temp = cardsTable[cardId].innerText;
+  let tempId = cardId;
+  let count2 = 0;
+  // console.log(tempId)
+  // console.log(cardId)
+  // console.log(cardsBack[cardId])
+  // attention: cardsBack is an array with value already, not an element grab from html, so dont use cardsBack[id].innertext
+  // if (count2 < 2) {
+    cardsTable[cardId].innerText = cardsBack[cardId];
+    cardsTable[cardId].style = `background-color: pink;`;
+    console.log(temp);
+    for (let i = 0; i < cardsTable.length; i++) {
+      if (i !== cardId) cardsTable[i].innerText = cardsBack[i];
+    }
+    cardsBack = cardsBackFill(cardsBack)
+    // count2++
+  // }
 };
 
 // start button is pressed
 start.addEventListener("click", cardsFill);
-console.log(
-  `${shuffledCards} First display cards when Start button pressed`
-);
-console.log(`${cardsBack} Back of the cards was called `);
 
-// eventListener for every box in the table
+//eventListener for every box in the table
 cardsTable.forEach((card) => {
-    card.addEventListener("click", cardClicked)
-})
-
-function cardClicked() {
-    console.log(`card click`)
-}
-
+  card.addEventListener("click", cardClicked);
+});
 
 // const cardClicked = (e) => {
 //     let id = e.target.id
-//     cardsTable[id].innerText = 
+//     cardsTable[id].innerText =
 // }
