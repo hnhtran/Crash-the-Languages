c4dac21346d70a21ac42a8f46f1835869c3e9c7b
hnhtran <tran_eetas@yahoo.com>
2022-05-26 06:06:41 +1000

    tests and bug fix

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 // Grab the neccessary elements
 let cardsTable = document.querySelectorAll(".cards");
 let home = document.querySelectorAll(".home");
-let board = document.getElementById('board');
-let data = document.getElementById('data')
+let board = document.getElementById("board");
+let data = document.getElementById("data");
+let row1 = document.getElementById("row1");
+let row2 = document.getElementById("row2");
 // console.log(cardsTable[0].innerText);
 const start = document.getElementById("shuffle");
 
@@ -49,7 +51,7 @@ function cardsBackFill(arr) {
       }
     }
   }
-};
+}
 
 // cardsFill will be called when Start button is pressed, and empty table will be filled with words
 function cardsFill() {
@@ -64,18 +66,18 @@ function cardsFill() {
   cardsBackFill(shuffledCards);
   // console.log(`${cardsBack} Back of the cards was called `);
   cardsSwap = cardsBack;
-};
+}
 
 // restart here, reset table, reset board result
 let countMatch = 0;
 let tempIdArr = [];
 function restart() {
   countMatch = 0;
-  tempIdArr=[]
-  
+  tempIdArr = [];
+
   //reset border of the 2 tables
-  board.removeAttribute('class')
-  data.setAttribute('class', 'table table-bordered')
+  board.removeAttribute("class");
+  data.setAttribute("class", "table table-bordered");
 
   for (let i = 0; i < cardsTable.length; i++) {
     // cardsTable[i].style = `background-color: none;`;
@@ -85,19 +87,18 @@ function restart() {
   }
   //remove td from board
   for (let i = 0; i < 4; i++) {
-    board.children[0].children[i].innerHTML=``
+    board.children[0].children[i].innerHTML = ``;
   }
-  
+
   cardsFill();
-  cardClickListen()
-};
+  cardClickListen();
+}
 
 let temp = [];
 let tempId = [];
 function match(temp, tempId) {
-  board.setAttribute('class', 'table table-bordered')
+  board.setAttribute("class", "table table-bordered");
   if (temp[0] === temp[1]) {
-
     board.children[0].children[countMatch].innerHTML = `<td></td> <td></td>`;
 
     let td1 = board.children[0].children[countMatch].children[0];
@@ -110,14 +111,31 @@ function match(temp, tempId) {
 
     home[tempId[0]].removeEventListener("click", cardClicked);
     home[tempId[1]].removeEventListener("click", cardClicked);
+
     countMatch++;
+
+    if (countMatch === 3) {
+      home[tempId[0]].addEventListener("click", cardClicked);
+      home[tempId[1]].addEventListener("click", cardClicked);
+    }
+    console.log(countMatch);
+    if (countMatch === 4) {
+      //remove table border
+      data.removeAttribute("class");
+      // row1.innerHTML = ``;
+      // row2.innerHTML = ``;
+    }
+  } else {
+    home[tempId[0]].addEventListener("click", cardClicked);
+    home[tempId[1]].addEventListener("click", cardClicked);
   }
-};
+}
 
 let count2 = 0;
 function cardClicked(e) {
   let cardId = e.target.id;
-  
+  // console.log(`${countMatch} reach`)
+
   // console.log(
   //   `${cardId} clicked, with the word ${cardsTable[cardId].innerText}`
   // ); // cannot do cardId.innerText, maybe not exist.. shoudl be cardsTable[id]?
@@ -146,7 +164,9 @@ function cardClicked(e) {
     // console.log(temp);
 
     for (let i = 0; i < cardsTable.length; i++) {
-      if (i !== cardId) {cardsTable[i].innerText = cardsSwap[i]};
+      if (i !== cardId) {
+        cardsTable[i].innerText = cardsSwap[i];
+      }
     }
     // reset cards at the back as cardsTable, except value of the card at cardId
     if (cardsSwap === shuffledCards) {
@@ -155,7 +175,7 @@ function cardClicked(e) {
       cardsSwap = shuffledCards;
     }
 
-    // home[cardId].removeEventListener("click", cardClicked);
+    home[cardId].removeEventListener("click", cardClicked);
     count2++;
   } else {
     match(temp, tempId);
@@ -169,25 +189,22 @@ function cardClicked(e) {
       home[i].style = `background-color: none;`;
     }
   }
-};
-
-
+}
 
 //eventListener for every box in the table
 function cardClickListen() {
   // if (countMatch < 4) {
-    home.forEach((card) => {
-      // if cardId === tempIdArray, remove event listener, except 0s
-      // console.log(tempIdArr.length)
-      // if (tempIdArr.length === 0) {
-        card.addEventListener("click", cardClicked);
-      // } 
-    });
+  home.forEach((card) => {
+    // if cardId === tempIdArray, remove event listener, except 0s
+    // console.log(tempIdArr.length)
+    // if (tempIdArr.length === 0) {
+    card.addEventListener("click", cardClicked);
+    // }
+  });
   // }
 }
 
 restart();
 // start button is pressed
 start.addEventListener("click", restart);
-cardClickListen()
-
+cardClickListen();
